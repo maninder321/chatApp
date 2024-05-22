349ff100a28a5fc705b56fe9dad134f65e49b5e8
maninder321 <maninderjitsingh380@gmail.com>
2024-05-22 10:57:27 +0530

    --added navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -3,22 +3,27 @@ import "./css/styles.css";
 function Navbar() {
   return (
     <div className="navbarChat">
-      <div className="profile mt-3">
-        <img src="https://picsum.photos/id/237/200/300" />
-        <div className="activeStatus online"></div>
-      </div>
-      <div className="navItems my-5">
-        <div className="navItem">
-          <i className="fa-solid fa-house-chimney"></i>
+      <div className="navWrapper">
+        <div className="logo mt-3">
+          <i className="fa-solid fa-ring"></i>
         </div>
-        <div className="navItem">
-          <i className="fa-solid fa-inbox"></i>
+        <div className="profile mt-2">
+          <img src="https://picsum.photos/id/237/200/300" />
+          <div className="activeStatus online"></div>
         </div>
-        <div className="navItem">
-          <i className="fa-solid fa-bell"></i>
-        </div>
-        <div className="navItem">
-          <i className="fa-solid fa-gear"></i>
+        <div className="navItems my-4">
+          <div className="navItem">
+            <i className="fa-solid fa-house-chimney"></i>
+          </div>
+          <div className="navItem">
+            <i className="fa-solid fa-inbox"></i>
+          </div>
+          <div className="navItem">
+            <i className="fa-solid fa-bell"></i>
+          </div>
+          <div className="navItem">
+            <i className="fa-solid fa-gear"></i>
+          </div>
         </div>
       </div>
       <div className="extraNavItems my-4">
