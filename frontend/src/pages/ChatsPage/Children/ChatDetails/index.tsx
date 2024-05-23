44b6c4b735fbcdf666fa7577d7f598e80a569a6c
maninder321 <maninderjitsingh380@gmail.com>
2024-05-23 11:00:29 +0530

    --added structure for chat details componenet

--- frontend/src/pages/ChatsPage/Children/ChatDetails/index.tsx
+++ frontend/src/pages/ChatsPage/Children/ChatDetails/index.tsx
@@ -1,7 +1,13 @@
 import "./css/styles.css";
 
 function ChatDetails() {
-  return <div>ChatDetails</div>;
+  return (
+    <div className="chatDetailsWrapper">
+      <div className="chatDetailsHeader">Header</div>
+      <div className="chatDetailsMain">Main</div>
+      <div className="chatDetailsFooter">Footer</div>
+    </div>
+  );
 }
 
 export default ChatDetails;
