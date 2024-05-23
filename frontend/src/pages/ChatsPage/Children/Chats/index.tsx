8434259eab057abb00b9984f683941910e381141
maninder321 <maninderjitsingh380@gmail.com>
2024-05-23 09:25:27 +0530

    --added header and search bar for chats

--- frontend/src/pages/ChatsPage/Children/Chats/index.tsx
+++ frontend/src/pages/ChatsPage/Children/Chats/index.tsx
@@ -1,7 +1,20 @@
 import "./css/styles.css";
 
 function Chats() {
-  return <div>Chats</div>;
+  return (
+    <div className="chatsWrapper">
+      <div className="title">
+        <span>Chats</span>
+        <div className="addIcon">
+          <i className="fa-solid fa-circle-plus"></i>
+        </div>
+      </div>
+      <div className="searchInput">
+        <i className="fa-solid fa-magnifying-glass"></i>
+        <input className="form-control" placeholder="Search" type="text" />
+      </div>
+    </div>
+  );
 }
 
 export default Chats;
--- frontend/src/pages/ChatsPage/index.tsx
+++ frontend/src/pages/ChatsPage/index.tsx
@@ -1,5 +1,18 @@
+import ChatDetails from "./Children/ChatDetails";
+import Chats from "./Children/Chats";
+import "./css/styles.css";
+
 function ChatsPage() {
-  return <div>ChatsPage</div>;
+  return (
+    <div className="chatsPageWrapper">
+      <div className="leftSection">
+        <Chats />
+      </div>
+      <div className="rightSection">
+        <ChatDetails />
+      </div>
+    </div>
+  );
 }
 
 export default ChatsPage;
