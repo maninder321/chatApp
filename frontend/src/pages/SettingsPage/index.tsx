e978d26cf45d0ae16464625c989512e838c4c94d
maninder321 <maninderjitsingh380@gmail.com>
2024-05-31 23:02:20 +0530

    added left sidebar for setting page

--- frontend/src/pages/SettingsPage/index.tsx
+++ frontend/src/pages/SettingsPage/index.tsx
@@ -1,5 +1,42 @@
+import Logo from "../../components/Logo";
+import styles from "./css/settingPage.module.css";
+
 function SettingsPage() {
-  return <div>SettingsPage</div>;
+  return (
+    <div className={styles.settingWrapper}>
+      <div className={styles.leftSettingSection}>
+        <Logo />
+        <div className={styles.settingTitle}>Settings</div>
+        <div className={styles.settingItems}>
+          <div className={`${styles.settingItem} ${styles.settingItemActive}`}>
+            <div className={styles.settingItemLogo}>
+              <i className="fa-solid fa-user"></i>
+            </div>
+            <div className={styles.settingItemTitle}>Profile</div>
+          </div>
+          <div className={styles.settingItem} style={{ cursor: "not-allowed" }}>
+            <div className={styles.settingItemLogo}>
+              <i className="fa-solid fa-shield-halved"></i>
+            </div>
+            <div className={styles.settingItemTitle}>Security</div>
+          </div>
+          <div className={styles.settingItem} style={{ cursor: "not-allowed" }}>
+            <div className={styles.settingItemLogo}>
+              <i className="fa-solid fa-lock"></i>
+            </div>
+            <div className={styles.settingItemTitle}>Account</div>
+          </div>
+          <div className={styles.settingItem} style={{ cursor: "not-allowed" }}>
+            <div className={styles.settingItemLogo}>
+              <i className="fa-solid fa-globe"></i>
+            </div>
+            <div className={styles.settingItemTitle}>Language</div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.rightSettingSection}>Right Section</div>
+    </div>
+  );
 }
 
 export default SettingsPage;
