14940b5d197ab84d0d6094f674e411bc23b96db1
maninder321 <maninderjitsingh380@gmail.com>
2024-05-31 23:35:08 +0530

    --added profile setting

--- frontend/src/pages/SettingsPage/Children/ProfileSetting/index.tsx
+++ frontend/src/pages/SettingsPage/Children/ProfileSetting/index.tsx
@@ -1,7 +1,17 @@
 import styles from "./css/profileSetting.module.css";
 
 function ProfileSetting() {
-  return <div>ProfileSetting</div>;
+  return (
+    <div className={`${styles.profileSettingWrapper}`}>
+      <div className={`${styles.profileHeader}`}>
+        <span>Profile Settings</span>
+      </div>
+      <div className={`${styles.profileBody}`}>
+        <div className="profileLeft">left</div>
+        <div className="profileRight">right</div>
+      </div>
+    </div>
+  );
 }
 
 export default ProfileSetting;
