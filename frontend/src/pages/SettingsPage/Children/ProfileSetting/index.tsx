bc06a677ef3d41e690fe8aa517e74013cb5aed83
maninder321 <maninderjitsingh380@gmail.com>
2024-06-01 21:30:15 +0530

    --added profile setting desgin fixes

--- frontend/src/pages/SettingsPage/Children/ProfileSetting/index.tsx
+++ frontend/src/pages/SettingsPage/Children/ProfileSetting/index.tsx
@@ -6,42 +6,53 @@ function ProfileSetting() {
       <div className={`${styles.profileHeader}`}>
         <span>Profile Settings</span>
       </div>
-      <div className={`${styles.profileBody}`}>
-        <div className={`${styles.profileImageSection}`}>
-          <img
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0XIQ_GMQoiF3GYL4ck4gpZCPaXf4dRW3Chw&s"
-            alt=""
-          />
-          <div className={`${styles.profileImageChangeButton}`}>
-            <i className="fa-solid fa-pen-to-square"></i>
-            <span>Edit Profile</span>
+      <div className={`${styles.profileContainer}`}>
+        <div className={`${styles.profileBody}`}>
+          <div className={`${styles.profileImageSection}`}>
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0XIQ_GMQoiF3GYL4ck4gpZCPaXf4dRW3Chw&s"
+              alt=""
+            />
+            <div className={`${styles.profileImageChangeButton}`}>
+              <i className="fa-solid fa-pen-to-square"></i>
+              <span>Edit Profile</span>
+            </div>
           </div>
-        </div>
-        <div className={`${styles.profileSettingForm}`}>
-          <input
-            type="text"
-            id="profileName"
-            className="form-control"
-            placeholder="Name"
-            value="Maninder"
-            disabled
-          />
-          <input
-            type="text"
-            id="profileUsername"
-            className="form-control"
-            placeholder="Username"
-            value={"maninder7463"}
-            disabled
-          />
-          <input
-            type="text"
-            id="profileEmail"
-            className="form-control"
-            placeholder="E-mail"
-            value={"maninderjitsingh380@gmail.com"}
-            disabled
-          />
+          <div className={`${styles.profileSettingForm}`}>
+            <div className={`${styles.profileInput}`}>
+              <span className={`${styles.inputTitle}`}>Name</span>
+              <input
+                type="text"
+                id="profileName"
+                className="form-control"
+                placeholder="Name"
+                value="Maninder"
+                // disabled
+              />
+            </div>
+            <div className={`${styles.profileInput}`}>
+              <span className={`${styles.inputTitle}`}>Username</span>
+              <input
+                type="text"
+                id="profileUsername"
+                className="form-control"
+                placeholder="Username"
+                value={"maninder7463"}
+                // disabled
+              />
+            </div>
+            <div className={`${styles.profileInput}`}>
+              <span className={`${styles.inputTitle}`}>E-mail</span>
+              <input
+                type="text"
+                id="profileEmail"
+                className="form-control"
+                placeholder="E-mail"
+                value={"maninderjitsingh380@gmail.com"}
+                // disabled
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
