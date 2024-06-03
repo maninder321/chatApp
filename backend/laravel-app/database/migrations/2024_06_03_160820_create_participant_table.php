03877aeb98b8af0da900d1c990f5cd5dd09da41e
maninder321 <maninderjitsingh380@gmail.com>
2024-06-03 21:58:50 +0530

    --added conversations, conversation_messages, participants models and migrations

---
+++ backend/laravel-app/database/migrations/2024_06_03_160820_create_participant_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->bigInteger("id")->autoIncrement();
+            $table->bigInteger("conversation_id");
+            $table->longText("user_ids");
+            $table->datetime("created_at_gmt")->useCurrent();
+            $table->datetime("updated_at_gmt")->useCurrent();
+            $table->tinyInteger("deleted")->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('participant');
+    }
+};
